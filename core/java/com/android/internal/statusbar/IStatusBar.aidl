a702534f1d604187948d4abc8d862d2d4a3614c2
ezio84 <brabus84@gmail.com>
2016-10-11 16:33:17 +0200

    Add NavBar toggle [1/2]
    todo: createNavigationBar try-catch gives protection against
    a monkey toggling the navbar switch more times and quickly
    (NavigationBarFragment create() could try to add the view
    navigationBarView while the old one still exists) but it's not
    the best solution, it'd better to check if the activity isFinishing
    or something similar
    
    Change-Id: I0050e8e91b48ebd3ff1c313a0830f3ec17f94ad8

--- core/java/com/android/internal/statusbar/IStatusBar.aidl
+++ core/java/com/android/internal/statusbar/IStatusBar.aidl
@@ -127,4 +127,6 @@ oneway interface IStatusBar
     void handleSystemKey(in int key);
 
     void showShutdownUi(boolean isReboot, String reason, boolean rebootCustom);
+
+    void toggleNavigationBar(boolean enable);
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -84,6 +84,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SET_TOP_APP_HIDES_STATUS_BAR  = 37 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED  = 38 << MSG_SHIFT;
     private static final int MSG_TOGGLE_FLASHLIGHT             = 39 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_NAVIGATION_BAR         = 40 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -146,6 +147,7 @@ public class CommandQueue extends IStatusBar.Stub {
         default void handleShowShutdownUi(boolean isReboot, String reason, boolean rebootCustom) { }
         default void screenPinningStateChanged(boolean enabled) {}
         default void toggleFlashlight() {}
+        default void toggleNavigationBar(boolean enable) { }
     }
 
     @VisibleForTesting
@@ -477,6 +479,13 @@ public class CommandQueue extends IStatusBar.Stub {
         }
     }
 
+    public void toggleNavigationBar(boolean enable) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_NAVIGATION_BAR);
+            mHandler.obtainMessage(MSG_TOGGLE_NAVIGATION_BAR, enable ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -681,6 +690,11 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_TOGGLE_FLASHLIGHT:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleFlashlight();
+                    }
+                    break;
+                case MSG_TOGGLE_NAVIGATION_BAR:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleNavigationBar(msg.arg1 != 0);
                     }
                     break;
             }
