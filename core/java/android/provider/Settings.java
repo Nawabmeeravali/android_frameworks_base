ef00780e8f5b06efc78459b30d8e17e203d3dac9
Lucchetto <lucchetto.tie@live.com>
2018-03-14 20:58:39 +0100

    Revert "Add three-fingers-swipe to screenshot [1/2]"
    This reverts commit 118f69009a524859ecc502a793ad0d7fdb16f334.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10351,12 +10351,6 @@ public final class Settings {
                 "storage_settings_clobber_threshold";
 
         /**
-         * Three Finger Gesture from Oppo
-         * @hide
-         */
-        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
