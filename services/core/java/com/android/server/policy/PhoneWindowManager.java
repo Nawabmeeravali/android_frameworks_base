839fe96fbdef650b63367427d171f59ca7454826
Lucchetto <lucchetto.tie@live.com>
2018-03-30 18:34:16 +0200

    Revert "Hardware key disable support"
    This reverts commit 709f0bfe3b73118527b9046e7c47ccd26b1af213.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6523,10 +6523,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    private boolean isHwKeysDisabled() {
-        return mKeyHandler != null ? mKeyHandler.isHwKeysDisabled() : false;
-    }
-
     /** {@inheritDoc} */
     @Override
     public int interceptKeyBeforeQueueing(KeyEvent event, int policyFlags) {
@@ -6611,8 +6607,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
-                && event.getRepeatCount() == 0
-                && !isHwKeysDisabled();
+                && event.getRepeatCount() == 0;
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
