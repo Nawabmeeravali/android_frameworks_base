e3a68b3d130a3f706630245fca896a24407a5a97
nilac8991 <>
2018-05-09 16:06:11 +0200

    [SQUASHED] Implement XOSP Blur personalization options (1/2)

--- packages/SystemUI/src/com/android/systemui/recents/Recents.java
+++ packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -340,6 +340,8 @@ public class Recents extends SystemUI
             return;
         }
 
+        RecentsActivity.startBlurTask();
+
         int growTarget = getComponent(Divider.class).getView().growsRecents();
 
         int currentUser = sSystemServicesProxy.getCurrentUser();
